036c43570b5ae4acc08702357a5d63d486843a3f
Slixz0 <90061842+slixz0123@users.noreply.github.com>
2023-04-23 15:11:15 -0500

    sidebar sin personalizar

---
+++ src/app/shared/Services/cargarjs-templates.service.ts
@@ -0,0 +1,115 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CargarjsTemplatesService {
+
+  constructor() { }
+  // <script src="src/assets/vendor/apexcharts/apexcharts.min.js"></script>
+  // <script src="src/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  // <script src="src/assets/vendor/chart.js/chart.umd.js"></script>
+  // <script src="src/assets/vendor/echarts/echarts.min.js"></script>
+  // <script src="src/assets/vendor/quill/quill.min.js"></script>
+  // <script src="src/assets/vendor/simple-datatables/simple-datatables.js"></script>
+  // <script src="src/assets/vendor/tinymce/tinymce.min.js"></script>
+  // <script src="src/assets/vendor/php-email-form/validate.js"></script>
+  carga_apexcharts(archivos: string[]) {
+    
+    for (let archivo of archivos) {
+      let script = document.createElement("script");
+
+      script.src = "./assets/vendor/apexcharts" + archivo + ".js"
+      let body = document.getElementsByTagName("body")[0];
+      body.appendChild(script);
+    }
+  }
+  //
+  carga_boostrap(archivos: string[]) {
+    
+    for (let archivo of archivos) {
+      let script = document.createElement("script");
+
+      script.src = "./assets/vendor/bootstrap/js/" + archivo + ".js"
+      let body = document.getElementsByTagName("body")[0];
+      body.appendChild(script);
+    }
+  }
+   //
+   carga_mainjs(archivos: string[]) {
+    
+    for (let archivo of archivos) {
+      let script = document.createElement("script");
+
+      script.src = "./assets/js/" + archivo + ".js"
+      let body = document.getElementsByTagName("body")[0];
+      body.appendChild(script);
+    }
+  }
+  carga_chartjs(archivos: string[]) {
+    
+    for (let archivo of archivos) {
+      let script = document.createElement("script");
+
+      script.src = "./assets/vendor/chart.js/" + archivo + ".js"
+      let body = document.getElementsByTagName("body")[0];
+      body.appendChild(script);
+    }
+  }
+  carga_echarts(archivos: string[]) {
+    
+    for (let archivo of archivos) {
+      let script = document.createElement("script");
+
+      script.src = "./assets/vendor/echarts/" + archivo + ".js"
+      let body = document.getElementsByTagName("body")[0];
+      body.appendChild(script);
+    }
+  }
+  carga_quill(archivos: string[]) {
+    
+    for (let archivo of archivos) {
+      let script = document.createElement("script");
+
+      script.src = "./assets/vendor/quill/" + archivo + ".js"
+      let body = document.getElementsByTagName("body")[0];
+      body.appendChild(script);
+    }
+  }
+
+  carga_simpledatatables(archivos: string[]) {
+    
+    for (let archivo of archivos) {
+      let script = document.createElement("script");
+
+      script.src = "./assets/vendor/simple-datatables/" + archivo + ".js"
+      let body = document.getElementsByTagName("body")[0];
+      body.appendChild(script);
+    }
+  }
+  //tinymce
+
+  carga_tinymce(archivos: string[]) {
+    
+    for (let archivo of archivos) {
+      let script = document.createElement("script");
+
+      script.src = "./assets/vendor/tinymce/" + archivo + ".js"
+      let body = document.getElementsByTagName("body")[0];
+      body.appendChild(script);
+    }
+  }
+
+   ///assets/vendor/php-email-form/
+   carga_phpemailform(archivos: string[]) {
+    
+    for (let archivo of archivos) {
+      let script = document.createElement("script");
+
+      script.src = "./assets/vendor/php-email-form/" + archivo + ".js"
+      let body = document.getElementsByTagName("body")[0];
+      body.appendChild(script);
+    }
+  }
+  
+}
--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CargarjsTemplatesService } from '../../Services/cargarjs-templates.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent {
+  constructor(
+    private _CargarSc: CargarjsTemplatesService,
+    private router : Router
 
+
+  )
+  // <script src="src/assets/vendor/apexcharts/apexcharts.min.js"></script>
+  // <script src="src/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  // <script src="src/assets/vendor/chart.js/chart.umd.js"></script>
+  // <script src="src/assets/vendor/echarts/echarts.min.js"></script>
+  // <script src="src/assets/vendor/quill/quill.min.js"></script>
+  // <script src="src/assets/vendor/simple-datatables/simple-datatables.js"></script>
+  // <script src="src/assets/vendor/tinymce/tinymce.min.js"></script>
+  // <script src="src/assets/vendor/php-email-form/validate.js"></script>
+
+  {
+    {
+      _CargarSc.carga_apexcharts(["hapexcharts.min"])
+      _CargarSc.carga_boostrap(["bootstrap.bundle.min"])
+      _CargarSc.carga_mainjs(["main"])
+      _CargarSc.carga_chartjs(["chart.umd"])
+      _CargarSc.carga_echarts(["echarts.min"])
+      _CargarSc.carga_quill(["quill.min"])
+
+      _CargarSc.carga_simpledatatables(["simple-datatables"])
+      _CargarSc.carga_tinymce(["tinymce.min"])
+      _CargarSc.carga_phpemailform(["validate"])
+      
+      
+      
+
+    
+
+
+
+    }
+  }
 }
