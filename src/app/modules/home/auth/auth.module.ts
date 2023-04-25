83de46308a03834cd125673ad9070a611573a2e1
Slixz0 <90061842+slixz0123@users.noreply.github.com>
2023-04-24 20:09:24 -0500

    login

--- src/app/modules/home/auth/auth.module.ts
+++ src/app/modules/home/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './pages/auth/auth.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { AuthComponent } from './pages/auth/auth.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    SharedModule
   ],exports:[
     AuthComponent
   ]
