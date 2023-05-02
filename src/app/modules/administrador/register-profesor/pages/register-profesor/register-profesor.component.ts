733b55b082607fd5026bded182a7d809a97282a6
Chv0 <christian.argudo.est@tecazuay.edu.ec>
2023-05-01 20:29:08 -0500

    Avance Formulario profesores

--- src/app/modules/administrador/register-profesor/pages/register-profesor/register-profesor.component.ts
+++ src/app/modules/administrador/register-profesor/pages/register-profesor/register-profesor.component.ts
@@ -5,6 +5,7 @@ import { Usuario } from 'src/app/Core/models/usuario';
 import { PersonaServService } from 'src/app/shared/Services/persona-serv.service';
 import { RolServService } from 'src/app/shared/Services/rol-serv.service';
 import { UsuarioServService } from 'src/app/shared/Services/usuario-serv.service';
+import { claseValidaciones } from 'src/app/modules/utils/claseValidaciones';
 
 @Component({
   selector: 'app-register-profesor',
@@ -24,23 +25,49 @@ export class RegisterProfesorComponent {
 
   onSubmit() {
   
-    this.persoUsrService.postPersona(this.persona).subscribe(response => {
+    this.persoUsrService.postPersona(this.persona).subscribe((response:any) => {
       console.log(response); // Imprime la respuesta de la API en la consola
       this.persona.id_persona = response.id_persona; // a this.persona.id_persona el resultado de nuestro metodo post se aasignamos la data.id_persona que nos arroja la api
       this.usuario.persona = this.persona;// a  this.usuario.persona el resultado de nuestro metodo post se  asigna el  this.persona es decir el objeto 
 
 
-      this.rolservices.getById(2).subscribe(response => {
+      this.rolservices.getById(2).subscribe((response:any) => {
         console.log(response); // Imprime la respuesta de la API en la consola
         this.rol.id_rol = response.id_rol; // se asigna   a this.rol.rolId  la data arrojada por el metodo del servicio get asignandole  response.rolId;
         this.usuario.rol =response; // se accede a la relacion del rol en la clase usuario y se asigna la data encontrada del rol
 
-        this.userServiceService.postUsuario(this.usuario).subscribe(response => {
+        this.userServiceService.postUsuario(this.usuario).subscribe((response: any) => {
           console.log(response); // Imprime la respuesta de la API en la consola
         });
 
       });
 
     });
+
+    /////validar si la cédula es correcta de acuerdo a los parámetros     
+    const validar=new claseValidaciones();
+    if (this.persona.cedula !== undefined){
+      console.log(this.persona)
+      if(this.persona.cedula.length===10){
+        const esValida = validar.validarCedula(this.persona.cedula);
+        if(esValida){
+          alert('Lacédula es correcta');
+        }else{
+          alert('La cédula ingresada es incorrecta');
+        }
+      }else{
+        alert('La cédula debe contener exactamente 10 caracteres numéricos')
+      }
+    }else{
+      Error('La cédula no se encuentra en la base de datos')
+    }
+
+    if(this.usuario.username!== null){
+      const validaruser=validar.validarLetrasNumeros(this.usuario.username);
+        if(validaruser){
+
+        }
+      }
+    }
   }
-}
+
