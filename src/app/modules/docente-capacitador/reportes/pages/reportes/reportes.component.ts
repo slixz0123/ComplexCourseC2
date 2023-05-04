1b78e78d045d873fe7e0f2d7fb9a2f2ee971b2e5
Chv0 <christian.argudo.est@tecazuay.edu.ec>
2023-05-04 12:03:44 -0500

    Reportes finales

--- src/app/modules/docente-capacitador/reportes/pages/reportes/reportes.component.ts
+++ src/app/modules/docente-capacitador/reportes/pages/reportes/reportes.component.ts
@@ -5,6 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './reportes.component.html',
   styleUrls: ['./reportes.component.css']
 })
-export class ReportesComponent {
 
+export class ReportesComponent {
+  selectedForm: string = 'reportesEstudiantes';
+
+  ngOnInit(){
+
+  }
+
+  onSubmit(){
+
+  }
+  
+  onSearchInputChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const searchTerm = inputElement.value;
+    this.buscar(searchTerm);
+  }
+  buscar(valor: string) {
+    const filas = document.getElementsByTagName("tr");
+    for (let i = 0; i < filas.length; i++) {
+      const celdas = filas[i].getElementsByTagName("td");
+      for (let j = 0; j < celdas.length; j++) {
+        if (celdas[j].innerHTML.toUpperCase().indexOf(valor.toUpperCase()) > -1) {
+          filas[i].style.display = "";
+          break;
+        } else {
+          filas[i].style.display = "none";
+        }
+      }
+    }
+  }
 }
