45cbf68bcdb8fe7c45360c603d24a3e4b6366c1e
isaacMxS <isaac.mejia.est@tecazuay.edu.ec>
2023-05-06 22:51:56 -0500

    cambios de errores en las vistas ok

--- src/app/modules/super-admin/register-admins/pages/register-admins/register-admins.component.ts
+++ src/app/modules/super-admin/register-admins/pages/register-admins/register-admins.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { window } from 'rxjs';
 import { Persona } from 'src/app/Core/models/persona';
 import { Rol } from 'src/app/Core/models/rol';
 import { Usuario } from 'src/app/Core/models/usuario';
@@ -9,21 +10,21 @@ import { UsuarioServService } from 'src/app/shared/Services/usuario-serv.service
 @Component({
   selector: 'app-register-admins',
   templateUrl: './register-admins.component.html',
-  styleUrls: ['./register-admins.component.css']
+  styleUrls: ['./register-admins.component.css'],
 })
 export class RegisterAdminsComponent {
- 
-  persona: Persona = new Persona; // instancia de la clase persona 
-  usuario: Usuario = new Usuario; // instancia de la clase usuario  
-  rol: Rol = new Rol; // instancia de la clase rol 
+  persona: Persona = new Persona(); // instancia de la clase persona
+  usuario: Usuario = new Usuario(); // instancia de la clase usuario
+  rol: Rol = new Rol(); // instancia de la clase rol
 
-  
   // en el constructor instanciamos los servicios
-  constructor(private registerUsrService: PersonaServService, private userServiceService: UsuarioServService, private rolservices:RolServService) { }
+  constructor(
+    private registerUsrService: PersonaServService,
+    private userServiceService: UsuarioServService,
+    private rolservices: RolServService
+  ) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 
   resetForm() {
     this.persona = {
@@ -38,7 +39,7 @@ export class RegisterAdminsComponent {
       celular: '',
       nivelintruccion: '',
     };
-  
+
     this.usuario = {
       username: '',
       password: '',
@@ -46,35 +47,26 @@ export class RegisterAdminsComponent {
       rol: {},
     };
   }
-  
+
   onSubmitGuardar() {
-  
-    this.registerUsrService.postPersona(this.persona).subscribe(response => {
+    this.registerUsrService.postPersona(this.persona).subscribe((response) => {
       console.log(response); // Imprime la respuesta de la API en la consola
       this.persona.id_persona = response.id_persona; // a this.persona.id_persona el resultado de nuestro metodo post se aasignamos la data.id_persona que nos arroja la api
-      this.usuario.persona = this.persona;// a  this.usuario.persona el resultado de nuestro metodo post se  asigna el  this.persona es decir el objeto 
+      this.usuario.persona = this.persona; // a  this.usuario.persona el resultado de nuestro metodo post se  asigna el  this.persona es decir el objeto
 
-
-      this.rolservices.getById(3).subscribe(response => {
+      this.rolservices.getById(3).subscribe((response) => {
         console.log(response); // Imprime la respuesta de la API en la consola
         this.rol.id_rol = response.id_rol; // se asigna   a this.rol.rolId  la data arrojada por el metodo del servicio get asignandole  response.rolId;
-        this.usuario.rol =response; // se accede a la relacion del rol en la clase usuario y se asigna la data encontrada del rol
+        this.usuario.rol = response; // se accede a la relacion del rol en la clase usuario y se asigna la data encontrada del rol
 
-        this.userServiceService.postUsuario(this.usuario).subscribe(response => {
-          console.log(response); // Imprime la respuesta de la API en la consola
-        });
-
+        this.userServiceService
+          .postUsuario(this.usuario)
+          .subscribe((response) => {
+            console.log(response); // Imprime la respuesta de la API en la consola
+            // Llama a la función resetForm() para vaciar los campos del formulario
+            this.resetForm();
+          });
       });
-
-      
-
-    });
-    this.userServiceService.postUsuario(this.usuario).subscribe(response => {
-      console.log(response); // Imprime la respuesta de la API en la consola
-  
-      // Llama a la función resetForm() para vaciar los campos del formulario
-      this.resetForm();
     });
   }
-
-  }
+}
