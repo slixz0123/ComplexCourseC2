15bc662a51f8aeee4d595753bb96f3e9f941ec4b
Jose Illescas <jose.illescas.est@tecazuay.edu.ec>
2023-05-28 05:03:19 -0500

    Corrección ortográfica

--- src/app/modules/participantes/participantes-routing.module.ts
+++ src/app/modules/participantes/participantes-routing.module.ts
@@ -11,19 +11,19 @@ const routes: Routes = [
     path: 'vista-perfil-participante',
     loadChildren: () => import("./vista-perfil-participante/vista-perfil-participante.module").then(m => m.VistaPerfilParticipanteModule)
   },
-    //reportes
-    {
-      path: '',
-      loadChildren: () => import("./bienvenida-participantes/bienvenida-participantes.module").then(m => m.BienvenidaParticipantesModule)
-    },
-    //
-    {
-      path: 'Eva-Curso',
-      loadChildren: () => import("./evaluacion-curso/evaluacion-curso.module").then(m => m.EvaluacionCursoModule)
-    },
-    // Ficha de inscripcion
-  
-   {
+  //reportes
+  {
+    path: '',
+    loadChildren: () => import("./bienvenida-participantes/bienvenida-participantes.module").then(m => m.BienvenidaParticipantesModule)
+  },
+  //
+  {
+    path: 'Eva-Curso',
+    loadChildren: () => import("./evaluacion-curso/evaluacion-curso.module").then(m => m.EvaluacionCursoModule)
+  },
+  // Ficha de inscripcion
+
+  {
     path: 'ficha-inscripcion',
     loadChildren: () => import("./ficha-inscripcion/ficha-inscripcion.module").then(m => m.FichaInscripcionModule)
   },
@@ -32,7 +32,7 @@ const routes: Routes = [
     loadChildren: () => import("./gen-fincha-inscripcion/gen-fincha-inscripcion.module").then(m => m.GenFinchaInscripcionModule)
   },
   // ver programas de capcaitcacion y aplicar 
-  
+
   {
     path: 'programas-capacitacion',
     loadChildren: () => import("./visualizar-programas--capacitacion/visualizar-programas--capacitacion.module").then(m => m.VisualizarProgramasCapacitacionModule)
@@ -42,20 +42,15 @@ const routes: Routes = [
     loadChildren: () => import("./apli-cursos/apli-cursos.module").then(m => m.ApliCursosModule)
   },
   // ver cusrspos aplicados
-  
+
   {
     path: 'cursos-aplicados',
     loadChildren: () => import("./cursos-aplicados/cursos-aplicados.module").then(m => m.CursosAplicadosModule)
   },
-// REPORTE CURSOS-APROBADOS 
-{
-  path: 'certif-curso-aprovee',
-  loadChildren: () => import("./reporte-certificado-curso-aprobados/reporte-certificado-curso-aprobados.module").then(m => m.ReporteCertificadoCursoAprobadosModule)
-},
-{
-  path: 'ver-reglamento',
-  loadChildren:() => import("./ver-reglamento/ver-reglamento.module").then(m=>m.VerReglamentoModule)
-},
+  {
+    path: 'ver-reglamento',
+    loadChildren: () => import("./ver-reglamento/ver-reglamento.module").then(m => m.VerReglamentoModule)
+  },
 ];
 
 @NgModule({
--- src/app/shared/components/side-bar-participante/side-bar-participante.component.ts
+++ src/app/shared/components/side-bar-participante/side-bar-participante.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CargarjsTemplatesService } from '../../Services/cargarjsTemplates.service';
 import { Router } from '@angular/router';
+import { UsuarioService } from '../../Services/usuario.service';
 
 @Component({
   selector: 'app-side-bar-participante',
@@ -9,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class SideBarParticipanteComponent {
   constructor(
-    private _CargarSc: CargarjsTemplatesService, private router : Router)
+    private _CargarSc: CargarjsTemplatesService, private router : Router, private usuarioser: UsuarioService)
   {
       _CargarSc.carga_apexcharts(["apexcharts.min"])
       _CargarSc.carga_boostrap(["bootstrap.bundle.min"])
@@ -80,4 +81,39 @@ goToRegisterInscripcion($event: any) :void{
   ngOnInit(): void {
 
   }
+
+  id: any;
+     nombreUsuario: any;
+     nombreRol: any;
+   
+
+   
+     isSuperAdmin: boolean = false;
+     isClientAdmin: boolean = false;
+     isparticipante: boolean = false;
+     isadmin: boolean = false;
+     isdocente: boolean = false;
+   
+     displayMaximizable: any;
+     isLogin: boolean = false;
+     obtenerUsuario() {
+      this.id = localStorage.getItem('id_persona');
+      if (this.id != '' && this.id != undefined) {
+        this.usuarioser.getPersona(this.id).subscribe((data) => {
+          console.log(data);
+       
+          if (data != null) {
+            this.isLogin = true;
+    
+            this.nombreUsuario = data.nombre + ' ' + data.apellido;
+            
+            console.log(data.persona?.nombre + ' ' + data.persona?.apellido)
+    
+          } else {
+            this.isLogin = false;
+            this.nombreUsuario = 'NULL';
+          }
+        });
+      }
+    }
 }
