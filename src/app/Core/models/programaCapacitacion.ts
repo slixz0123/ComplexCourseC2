3c7148d7556c609bf56d3e233ddf4ff2b2065b98
Jose Illescas <jose.illescas.est@tecazuay.edu.ec>
2023-05-28 01:42:44 -0500

    solucion de validaciones y estilos

--- src/app/Core/models/programaCapacitacion.ts
+++ src/app/Core/models/programaCapacitacion.ts
@@ -1,8 +1,8 @@
 export class ProgramaCapacitacion{
     pcaId: number =0;
-    pcaNombre?: string;
-    pcaFechainicio?: Date;
-    pcaFechafin?: Date;
-    pcaProceso?: string;
+    pcaNombre!: string;
+    pcaFechainicio!: Date;
+    pcaFechafin!: Date;
+    pcaProceso!: string;
     pcaEstado?: boolean;
 }
--- src/app/modules/administrador/register-program-cap/pages/register-program-cap/register-program-cap.component.ts
+++ src/app/modules/administrador/register-program-cap/pages/register-program-cap/register-program-cap.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ProgramaCapacitacion } from 'src/app/Core/models/programaCapacitacion';
 import { ProgramaCapacitacionService } from 'src/app/shared/Services/programaCapacitacion.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register-program-cap',
@@ -11,30 +12,65 @@ export class RegisterProgramCapComponent {
   programaCapacitacion: ProgramaCapacitacion = new ProgramaCapacitacion; // instancia de la clase programa Capacitacion
   id_programac: any;
   estado: boolean = true;
+  curFechaIValido: boolean = true;
+  curFechaFValido: boolean = true;
+  curNombreValido: boolean = true;
+  procesoValido: boolean = true;
+
   constructor(
     private programaCapacitacionServ: ProgramaCapacitacionService
-    
-  ){
+
+  ) {
 
   }
 
   ngOnInit(): void {
-
     this.getAllProgramasc();
   }
+
   onSubmit() {
-    // this.id_programac=(this.programaCapacitacion.pcaId);
-    // console.log("esta es "+this.id_programac);
-    console.log(this.programaCapacitacion)
-    this.programaCapacitacion.pcaEstado=this.estado;
-    this.programaCapacitacionServ.createProgramaCapacitacion(this.programaCapacitacion).subscribe(   
-      (data: any) => {
-        console.log('a verrr' + data);
-      },
-      (err) => {
-        console.log(err);
-      }
-    );
+    const fechaInicio = new Date(this.programaCapacitacion.pcaFechainicio);
+    const fechaFin = new Date(this.programaCapacitacion.pcaFechafin);
+
+    const fechaInicioUTC = new Date(fechaInicio.getUTCFullYear(), fechaInicio.getUTCMonth(), fechaInicio.getUTCDate());
+    const fechaFinUTC = new Date(fechaFin.getUTCFullYear(), fechaFin.getUTCMonth(), fechaFin.getUTCDate());
+
+    this.programaCapacitacion.pcaFechafin = fechaInicioUTC;
+    this.programaCapacitacion.pcaFechafin = fechaFinUTC;
+    const nombreRegex = /^[a-zA-Z0-9\s.,;:!"#$%&'()*+\-\/<=>?@[\\\]^_`{|}~áéíóúÁÉÍÓÚ]+$/;
+    this.curNombreValido = nombreRegex.test(this.programaCapacitacion.pcaNombre);
+    // Verificar que la fecha de inicio no sea menor o igual a la fecha actual del sistema
+    this.curFechaIValido = new Date(this.programaCapacitacion.pcaFechainicio) > new Date();
+
+    // Verificar que la fecha de fin sea mayor a la fecha de inicio
+    this.curFechaFValido = new Date(this.programaCapacitacion.pcaFechafin) > new Date(this.programaCapacitacion.pcaFechainicio);
+
+    if (!this.programaCapacitacion.pcaProceso) {
+      this.procesoValido = false;
+      return;
+    } else {
+      this.procesoValido = true;
+    }
+    if (this.curNombreValido && this.curFechaIValido && this.curFechaFValido && this.procesoValido) {
+      this.programaCapacitacion.pcaEstado = this.estado;
+      this.programaCapacitacionServ.createProgramaCapacitacion(this.programaCapacitacion).subscribe(
+        (data: any) => {
+          Swal.fire('¡Éxito!', 'El programa se ha guardado correctamente', 'success')
+          this.getAllProgramasc();
+          this.programaCapacitacion.pcaNombre = '';
+          this.programaCapacitacion.pcaFechainicio = new Date();
+          this.programaCapacitacion.pcaFechafin = new Date();
+          this.programaCapacitacion.pcaProceso = '';
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    } else {
+      Swal.fire('Error', 'Datos incorrectos. Es necesario que llene todos los datos', 'error');
+
+    }
+    this.getAllProgramasc();
   }
   programasList: any[] = [];
   public getAllProgramasc() {
@@ -44,7 +80,14 @@ export class RegisterProgramCapComponent {
   }
 
   editarPrograma(programa: any) {
-    console.log(this.programaCapacitacion.pcaFechainicio)
+    const fechaInicio = new Date(this.programaCapacitacion.pcaFechainicio);
+    const fechaFin = new Date(this.programaCapacitacion.pcaFechafin);
+
+    const fechaInicioUTC = new Date(fechaInicio.getUTCFullYear(), fechaInicio.getUTCMonth(), fechaInicio.getUTCDate());
+    const fechaFinUTC = new Date(fechaFin.getUTCFullYear(), fechaFin.getUTCMonth(), fechaFin.getUTCDate());
+
+    this.programaCapacitacion.pcaFechafin = fechaInicioUTC;
+    this.programaCapacitacion.pcaFechafin = fechaFinUTC;
     const fechai = new Date(programa.pcaFechainicio);
     const fechaFormateadai = fechai.toISOString().slice(0, 10); // "2023-05-10"
     programa.pcaFechainicio = fechaFormateadai;
@@ -52,6 +95,7 @@ export class RegisterProgramCapComponent {
     const fechaFormateadaf = fechaf.toISOString().slice(0, 10); // "2023-05-10"
     programa.pcaFechafin = fechaFormateadaf;
     this.programaCapacitacion = programa;
+    this.getAllProgramasc();
   }
-  
+
 }
